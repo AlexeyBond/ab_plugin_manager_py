08eb7deffb95b46d282993a5ca17571f02519d7b
A.Bond <alexey.bond.94.55@gmail.com>
2025-04-09 15:24:22 +0600

    restore lost web server change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ab_plugin_manager",
-    version="0.1.7",
+    version="0.1.8",
 
     author="Alexey Bondarenko",
     author_email="alexey.bond.94.55@gmail.com",
